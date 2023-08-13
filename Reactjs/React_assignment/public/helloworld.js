68309b733f23ecaaffd574c6658a861458b67c7e
VihKag <nvkhang191@gmail.com>
2023-08-13 09:14:53 +0700

    update q2, q3 assignment

--- Reactjs/React_assignment/public/helloworld.js
+++ Reactjs/React_assignment/public/helloworld.js
@@ -6,17 +6,33 @@
 // myMap.set("MSSV111","Nguyen B");
 // console.log(myMap.size);
 // console.log(myMap.get("MSSV123"));
-function displayInput() {
-    var _a, _b;
-    var userInput = (_a = document.getElementById("inputString1")) === null || _a === void 0 ? void 0 : _a.textContent;
-    console.log("Bạn đã nhập: " + userInput);
-    var userInput2 = (_b = document.getElementById("inputString2")) === null || _b === void 0 ? void 0 : _b.textContent;
-    console.log("Bạn đã nhập: " + userInput2);
-}
+// function displayInput() {
+//     const userInput:any = document.getElementById("inputString1")?.textContent;
+//     const userInput2:any = document.getElementById("inputString2")?.textContent;
+//     // console.log("Bạn đã nhập: " + userInput2);
+// }
 function countChar() {
     var str = document.getElementById("inputString1");
     var result = str.value.replace(/\s+/g, '').length;
     console.log(result);
     var countString = document.getElementById("countString");
-    countString.innerHTML = "S\u1ED1 l\u01B0\u1EE3ng k\u00ED t\u1EF1: ".concat(result);
+    // countString.innerHTML = `Số lượng kí tự: ${result}`;
+    displayResult(countString, "Số lượng kí tự: ", result);
+}
+function displayResult(element, text, result) {
+    element.innerHTML = text + result;
+    console.log(text + result);
+}
+function upperName() {
+    var uppername = document.querySelector("#inputString1").value;
+    var result = (uppername === null || uppername === void 0 ? void 0 : uppername.charAt(0).toUpperCase()) + (uppername === null || uppername === void 0 ? void 0 : uppername.slice(1));
+    var upperstring = document.getElementById("upperString");
+    displayResult(upperstring, "Chỉnh sửa tên viết hoa chữ cái đầu: ", result);
+}
+function concateString() {
+    var concatchar = document.getElementById("concatChar");
+    var string1 = document.querySelector("#inputString1").value;
+    var string2 = document.querySelector("#inputString2").value;
+    var result = string1 === null || string1 === void 0 ? void 0 : string1.concat(" ", string2); //string1=string1+" "+string2
+    displayResult(concatchar, "Nối hai chuỗi s1 và s2: ", result);
 }
--- Reactjs/React_assignment/public/helloworld.ts
+++ Reactjs/React_assignment/public/helloworld.ts
@@ -8,12 +8,11 @@
 // console.log(myMap.size);
 // console.log(myMap.get("MSSV123"));
 
-function displayInput() {
-    const userInput:any = document.getElementById("inputString1")?.textContent;
-    console.log("Bạn đã nhập: " + userInput);
-    const userInput2:any = document.getElementById("inputString2")?.textContent;
-    console.log("Bạn đã nhập: " + userInput2);
-}
+// function displayInput() {
+//     const userInput:any = document.getElementById("inputString1")?.textContent;
+//     const userInput2:any = document.getElementById("inputString2")?.textContent;
+//     // console.log("Bạn đã nhập: " + userInput2);
+// }
 
 function countChar(){
     const str:any = document.getElementById("inputString1");
@@ -21,9 +20,24 @@ function countChar(){
     console.log(result);
     const countString:any = document.getElementById("countString");
     // countString.innerHTML = `Số lượng kí tự: ${result}`;
-    displayResult(countString, countString?.textContent,result);
+    displayResult(countString, "Số lượng kí tự: ",result);
 }
 
 function displayResult(element:any, text:string, result:any){
     element.innerHTML = text + result;
+    console.log(text + result)
+}
+
+function upperName(){
+    let uppername : any= (<HTMLInputElement>document.querySelector("#inputString1")).value;
+    const result:string = uppername?.charAt(0).toUpperCase()+uppername?.slice(1);
+    const upperstring:any = document.getElementById("upperString");
+    displayResult(upperstring, "Chỉnh sửa tên viết hoa chữ cái đầu: ",result);
+}
+function concateString(){
+    const concatchar :any = document.getElementById("concatChar");
+    let string1 : any= (<HTMLInputElement>document.querySelector("#inputString1")).value;
+    let string2 : any= (<HTMLInputElement>document.querySelector("#inputString2")).value;
+    const result:any= string1?.concat(" ", string2);//string1=string1+" "+string2
+    displayResult(concatchar, "Nối hai chuỗi s1 và s2: ",result)
 }
