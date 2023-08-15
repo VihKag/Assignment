aa4fa7f83e7115dec0266a4ac552f667acc0f9d3
VihKag <nvkhang191@gmail.com>
2023-08-15 18:31:34 +0700

    update assignment

--- Reactjs/React_assignment/public/helloworld.js
+++ Reactjs/React_assignment/public/helloworld.js
@@ -1,16 +1,4 @@
-// var message:string = "Hello world!!!"
-// var bool:boolean = true;
-// console.log(bool);
-// const myMap = new Map();
-// myMap.set("MSSV123", "Nguyen A");
-// myMap.set("MSSV111","Nguyen B");
-// console.log(myMap.size);
-// console.log(myMap.get("MSSV123"));
-// function displayInput() {
-//     const userInput:any = document.getElementById("inputString1")?.textContent;
-//     const userInput2:any = document.getElementById("inputString2")?.textContent;
-//     // console.log("Bạn đã nhập: " + userInput2);
-// }
+// count the number of characters in a string
 function countChar() {
     var str = document.getElementById("inputString1");
     var result = str.value.replace(/\s+/g, '').length;
@@ -23,12 +11,14 @@ function displayResult(element, text, result) {
     element.innerHTML = text + result;
     console.log(text + result);
 }
+// capitalize first letter
 function upperName() {
     var uppername = document.querySelector("#inputString1").value;
     var result = (uppername === null || uppername === void 0 ? void 0 : uppername.charAt(0).toUpperCase()) + (uppername === null || uppername === void 0 ? void 0 : uppername.slice(1));
     var upperstring = document.getElementById("upperString");
     displayResult(upperstring, "Chỉnh sửa tên viết hoa chữ cái đầu: ", result);
 }
+// Concatenate two character strings
 function concateString() {
     var concatchar = document.getElementById("concatChar");
     var string1 = document.querySelector("#inputString1").value;
@@ -36,6 +26,7 @@ function concateString() {
     var result = string1 === null || string1 === void 0 ? void 0 : string1.concat(" ", string2); //string1=string1+" "+string2
     displayResult(concatchar, "Nối hai chuỗi s1 và s2: ", result);
 }
+// capitalize each character in the string
 function upperCaseChar() {
     var outputupper = document.querySelector("#outputupper");
     var str = document.querySelector("#inputString1").value;
@@ -50,4 +41,36 @@ function createAndAppendElementHTML(tagName, parentElement, text) {
         element.textContent = text;
     }
     parentElement.appendChild(element);
+}
+// get full name
+function getFullName() {
+    var lastname = prompt("Nhập họ của bạn: ");
+    var firstname = prompt("Nhập tên của bạn: ");
+    var fullname = "".concat(lastname, " ").concat(firstname);
+    var displayFullName = document.querySelector("#fullName");
+    displayResult(displayFullName, "Họ tên: ", fullname);
+}
+// split full name
+function splitFullName() {
+    var outputsplitfullname = document.querySelector("#outputsplitfullname");
+    var fullname = prompt("Nhập họ tên: ");
+    var partsname = fullname.split(" ");
+    var firstname = partsname[partsname.length - 1];
+    var lastname = partsname[0];
+    var middlename = partsname.slice(1, partsname.length - 1).join(" ");
+    createAndAppendElementHTML("p", outputsplitfullname, "Ng\u01B0\u1EDDi d\u00F9ng nh\u1EADp t\u00EAn: ".concat(fullname));
+    createAndAppendElementHTML("p", outputsplitfullname, "H\u1ECD l\u00E0: ".concat(lastname));
+    createAndAppendElementHTML("p", outputsplitfullname, "T\u00EAn \u0111\u1EC7m l\u00E0: ".concat(middlename));
+    createAndAppendElementHTML("p", outputsplitfullname, "T\u00EAn l\u00E0: ".concat(firstname));
+}
+// format fullname
+function formatName() {
+    var name = document.querySelector("#outputformatname");
+    var fullname = prompt("Nhập họ tên: ");
+    var result = fullname
+        .trim()
+        .split(/\s+/)
+        .map(function (word) { return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(); })
+        .join(" ");
+    createAndAppendElementHTML("p", name, "H\u1ECD v\u00E0 t\u00EAn chu\u1EA9n h\u00F3a: ".concat(result));
 }
--- Reactjs/React_assignment/public/helloworld.ts
+++ Reactjs/React_assignment/public/helloworld.ts
@@ -1,19 +1,4 @@
-// var message:string = "Hello world!!!"
-// var bool:boolean = true;
-// console.log(bool);
-// const myMap = new Map();
-// myMap.set("MSSV123", "Nguyen A");
-// myMap.set("MSSV111","Nguyen B");
-
-// console.log(myMap.size);
-// console.log(myMap.get("MSSV123"));
-
-// function displayInput() {
-//     const userInput:any = document.getElementById("inputString1")?.textContent;
-//     const userInput2:any = document.getElementById("inputString2")?.textContent;
-//     // console.log("Bạn đã nhập: " + userInput2);
-// }
-
+// count the number of characters in a string
 function countChar(){
     const str:any = document.getElementById("inputString1");
     const result:any = str.value.replace(/\s+/g,'').length;
@@ -27,13 +12,14 @@ function displayResult(element:any, text:string, result:any){
     element.innerHTML = text + result;
     console.log(text + result)
 }
-
+// capitalize first letter
 function upperName(){
     let uppername : any= (<HTMLInputElement>document.querySelector("#inputString1")).value;
     const result:string = uppername?.charAt(0).toUpperCase()+uppername?.slice(1);
     const upperstring:any = document.getElementById("upperString");
     displayResult(upperstring, "Chỉnh sửa tên viết hoa chữ cái đầu: ",result);
 }
+// Concatenate two character strings
 function concateString(){
     const concatchar :any = document.getElementById("concatChar");
     let string1 : any= (<HTMLInputElement>document.querySelector("#inputString1")).value;
@@ -41,6 +27,7 @@ function concateString(){
     const result:any= string1?.concat(" ", string2);//string1=string1+" "+string2
     displayResult(concatchar, "Nối hai chuỗi s1 và s2: ",result)
 }
+// capitalize each character in the string
 function upperCaseChar(){
     const outputupper:any = <HTMLInputElement>document.querySelector("#outputupper");
     const str:any = (<HTMLInputElement>document.querySelector("#inputString1")).value;
@@ -58,5 +45,43 @@ function createAndAppendElementHTML(tagName:string, parentElement:HTMLElement, t
     parentElement.appendChild(element);
     
 }
+// get full name
+function getFullName(){
+    const lastname:any = prompt("Nhập họ của bạn: ");
+    const firstname : any = prompt("Nhập tên của bạn: ");
+    const fullname: string = `${lastname} ${firstname}`;
+    const displayFullName:any = document.querySelector("#fullName");
+    displayResult(displayFullName,"Họ tên: ", fullname);
+}
+
+// split full name
+function splitFullName(){
+    const outputsplitfullname:any=document.querySelector("#outputsplitfullname");
+    const fullname: any = prompt("Nhập họ tên: ");
+    const partsname: any[]=fullname.split(" ");
+    const firstname:string = partsname[partsname.length-1];
+    const lastname:string = partsname[0];
+    const middlename:any = partsname.slice(1,partsname.length-1).join(" ");
+    createAndAppendElementHTML("p",outputsplitfullname,`Người dùng nhập tên: ${fullname}`);
+    createAndAppendElementHTML("p",outputsplitfullname,`Họ là: ${lastname}`);
+    createAndAppendElementHTML("p",outputsplitfullname,`Tên đệm là: ${middlename}`);
+    createAndAppendElementHTML("p",outputsplitfullname,`Tên là: ${firstname}`);
+}
+
+// format fullname
+function formatName(){
+    const name:any=document.querySelector("#outputformatname");
+    const fullname:any=prompt("Nhập họ tên: ");
+    const result:string = fullname
+        .trim()
+        .split(/\s+/)
+        .map(word => word.charAt(0).toUpperCase()+word.slice(1).toLowerCase())
+        .join(" ");
+    createAndAppendElementHTML("p",name,`Họ và tên chuẩn hóa: ${result}`);
+
+}
+
+
+
 
 
